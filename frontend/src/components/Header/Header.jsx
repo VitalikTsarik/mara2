c37116bebcfad40e6dd8763e5d7bcfc6cc7d59b8
VitalikTsarik <VitalikTsarik@gmail.com>
2019-11-02 18:38:15 +0300

    Header template component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import classNames from 'classnames'
-
-import Logo from './logo.png'
+import classNames from 'classnames';
 
 import styles from './Header.scss';
+import grid from '../../styles/grid.scss';
+
+import Logo from './logo.png';
+import SearchBar from './SearchBar/SearchBar';
 
 const Header = () => {
     return (
-        <nav className={styles.header}>
+        <nav className={classNames(styles.header, grid.container)}>
             {/*<img className={styles.header_logo} src='/static/logo.png'/>*/}
-            <img className={styles.header_logo} src={Logo}/>
+            <img className={styles.header_logo} src={Logo} alt='logo' />
             <ul className={styles.header_navBar}>
                 <li className={styles.header_item}>Home</li>
                 <li className={styles.header_item}>List</li>
             </ul>
+            <SearchBar onSubmit={(value) => console.log(value)}/>
         </nav>
     );
 };
