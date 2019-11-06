25983641093c05ee995d24960d2675437a1914fc
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2019-11-06 21:24:26 +0300

    fixed callback deps

--- frontend/src/components/common/hooks.js
+++ frontend/src/components/common/hooks.js
@@ -1,19 +1,19 @@
 import { useCallback, useEffect } from 'react';
 
 const useClickOutside = (element, callback, enabled = true, target = document) => {
-  if (!enabled || !element)
-      return;
+    if (!enabled || !element)
+        return;
 
-  const handleClickOutside = useCallback((event) => {
-    if (element.current && !element.current.contains(event.target)) {
-      callback(event);
-    }
-  }, [callback]);
+    const handleClickOutside = useCallback((event) => {
+        if (element.current && !element.current.contains(event.target)) {
+            callback(event);
+        }
+    }, [callback, element]);
 
-  useEffect(() => {
-    target.addEventListener('click', handleClickOutside);
-    return () => target.removeEventListener('click', handleClickOutside);
-  }, [handleClickOutside, target]);
+    useEffect(() => {
+        target.addEventListener('click', handleClickOutside);
+        return () => target.removeEventListener('click', handleClickOutside);
+    }, [handleClickOutside, target]);
 };
 
 export default useClickOutside;
