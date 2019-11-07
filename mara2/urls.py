a3e4d908eaf835a26cdc3590a4a8d7d27b99648f
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2019-11-08 02:19:41 +0300

    remove unused views/viewsets

--- mara2/urls.py
+++ mara2/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', include('frontend.urls')),
     path('', include('subscriptions.urls')),
     path('admin/', admin.site.urls),
-    # path('', include('subscriptions.urls')),
     # path('registration/', user_views.RegistrationView.as_view(), name='registration'),
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,10 +1,5 @@
 from rest_framework.routers import DefaultRouter
 
-from .views.search import Search
-from .views.my_list import MyListView
-from .views.subscribe import subscribe
-from .views.tv_show_detail import TvShowDetailView
-
 from .api import TvShowViewSet, SubscriptionsViewSet
 
 router = DefaultRouter()
@@ -12,10 +7,3 @@ router.register('api/tv_show', TvShowViewSet)
 router.register('api/subscriptions', SubscriptionsViewSet)
 
 urlpatterns = router.urls
-
-#     [
-#     path('my_list/', MyListView.as_view(), name='my_list'),
-#     path('tv_show/<int:pk>', TvShowDetailView.as_view(), name='tv_show'),
-#     path('search/<search_input>', Search.as_view(), name='search'),
-#     path('subscribe/<int:tv_show_pk>', subscribe, name='subscribe'),
-# ]
