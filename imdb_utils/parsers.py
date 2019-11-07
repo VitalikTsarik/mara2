c0dc59a5fddad54608b1d8c8b8fe970d4bc8f4fa
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2019-11-08 02:11:49 +0300

    fix parsers

--- imdb_utils/parsers.py
+++ imdb_utils/parsers.py
@@ -1,30 +1,30 @@
-from subscriptions.models import TvShow
-from subscriptions.models import Watchable
+from subscriptions.models import TvShow, Watchable
+from datetime import timedelta
 
 
-def _imdb_movie_to_watchable(imdb_movie):
-    watchable = Watchable(imdb_id=imdb_movie['title'])
-    watchable.runtime = imdb_movie['runtimes'][0]
-    watchable.genres = ', '.join(imdb_movie['genres'])
-    watchable.year = str(imdb_movie['year'])
+def set_watchable_props(watchable, imdb_movie):
+    watchable.title = imdb_movie.get('title')
+    runtime = imdb_movie.get('runtimes')[0]
+    watchable.runtime = timedelta(minutes=int(runtime))
+    watchable.genres = ', '.join(imdb_movie.get('genres'))
+    watchable.year = str(imdb_movie.get('year'))
     watchable.imdb_id = int(imdb_movie.movieID)
-    watchable.poster_url = imdb_movie['cover url']
-    return watchable
+    watchable.poster_url = imdb_movie.get('cover url')
 
 
 def imdb_movie_to_tv_show(imdb_movie):
-    tv_show = _imdb_movie_to_watchable(imdb_movie)
-    tv_show.__class__ = TvShow
-
-    tv_show.seasons = imdb_movie['number of seasons']
-    tv_show.years = imdb_movie['series years']
+    tv_show = TvShow()
+    set_watchable_props(tv_show, imdb_movie)
+    tv_show.seasons = imdb_movie.get('number of seasons')
+    tv_show.years = imdb_movie.get('series years')
     return tv_show
 
 
 def search_result(result):
     movies = []
     for imdb_movie in result:
-        watchable = Watchable(imdd_id=int(imdb_movie.movieID))
-        watchable.year = str(imdb_movie['year'])
-        watchable.poster_url = imdb_movie['cover url']
+        watchable = Watchable()
+        watchable.year = str(imdb_movie.get('year'))
+        watchable.poster_url = imdb_movie.get('cover url')
         movies.append(watchable)
+    return movies
