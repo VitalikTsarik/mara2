47a69ecb99cec69053f29b3a16bb641379212036
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-01-08 01:26:25 +0300

    add styles to list

--- frontend/src/components/subscriptions/List/List.jsx
+++ frontend/src/components/subscriptions/List/List.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import styles from './List.scss';
+
 import Item from '../Item/Item';
 
 const List = ({data}) => {
     return (
-        <div>
+        <div className={styles.list}>
             {data.map((item) => (
-                    <Item
-                        key={item.content_id}
-                        item={item}
-                    />
+                    <div className={styles.item}>
+                        <Item
+                            key={item.content_id}
+                            item={item}
+                        />
+                    </div>
                 )
             )}
         </div>
