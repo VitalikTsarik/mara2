772468525dae363b569a36fce75a8b5673928cda
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-01-08 23:13:11 +0300

    add Page404

---
+++ frontend/src/components/Page404/Page404.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './Page404.scss'
+
+const Page404 = () => {
+    return (
+        <div className={styles.page404}>
+            <div className={styles.text404}>
+                404
+            </div>
+            <div>
+                Page not found
+            </div>
+        </div>
+    );
+};
+
+export default Page404;
