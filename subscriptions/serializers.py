b8ca734b370785f815e41ad832eee87ecc953c39
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-04-27 16:44:57 +0300

    add search serializers

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
-from .models import TvShow, Subscription
+from .models import TvShow, Subscription, Movie
 
 
 class TvShowDetailSerializer(ModelSerializer):
@@ -38,3 +39,40 @@ class SubscriptionsSerializer(ModelSerializer):
             'content',
             'subscription_date'
         )
+
+
+class TvShowSearchSerializer(ModelSerializer):
+    class Meta:
+        model = TvShow
+        fields = (
+            'content_id',
+            'title',
+            'seasons',
+            'poster_url',
+            'year',
+        )
+
+
+class MovieSearchSerializer(ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = (
+            'content_id',
+            'title',
+            'poster_url',
+            'year',
+        )
+
+
+class WatchableSearchSerializer(ModelSerializer):
+    def to_representation(self, instance):
+        instance_class = instance.get_real_instance_class()
+        if instance_class == Movie:
+            return MovieSearchSerializer(instance=instance).data
+        elif instance_class == TvShow:
+            return TvShowSearchSerializer(instance=instance).data
+        else:
+            raise ObjectDoesNotExist(f'There is no fitting a serializer for instance of {instance_class} ')
+
+    class Meta:
+        model = None
