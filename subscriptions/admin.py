605d06c640431e997501a2e6a4397bd826998967
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-05-02 21:53:13 +0300

    add admin action schedule_check_action for User

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from knox.auth import User
 
 from .models import Movie
 from .models import TvShow
 from .models import Subscription
+from .tasks import schedule_subscriptions_check
 
 admin.site.register(Subscription)
 admin.site.register(TvShow)
 admin.site.register(Movie)
+
+
+def schedule_check_action(admin, request, queryset):
+    for user in queryset:
+        schedule_subscriptions_check(user)
+
+
+schedule_check_action.short_description = "Schedule subscriptions check"
+
+
+class CustomUserAdmin(UserAdmin):
+    actions = [schedule_check_action]
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
